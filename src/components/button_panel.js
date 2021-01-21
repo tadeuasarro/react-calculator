8dcefb69af792d7ba27ff8263333c7314b52ae35
tadeuasarro <tadeutas@gmail.com>
2021-01-21 13:23:54 -0300

    Finish milestone2

--- src/components/button_panel.js
+++ src/components/button_panel.js
@@ -1,7 +1,26 @@
-const ButtonPanel = () => (
-  <div>
-    button panel
-  </div>
-);
+import Button from './button';
+
+const buttons = [
+  ['AC', '+/-', '%', '÷'],
+  ['7', '8', '9', 'X'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '='],
+];
+const buttonsNew = [];
+const ButtonPanel = () => {
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttonsNew.push(
+      <div key={`group + ${i}`}>
+        {
+          buttons[i].map(name => <Button key={name} name={name} />)
+        }
+      </div>,
+    );
+  }
+  return (
+    buttonsNew
+  );
+};
 
 export default ButtonPanel;
