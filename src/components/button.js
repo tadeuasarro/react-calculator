bc3f0a54fd1ed2b765da420c83df20b06dfe8dbf
tadeuasarro <tadeutas@gmail.com>
2021-01-26 17:55:03 -0300

    Finish styling

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ name, onClick }) => (
-  <button onClick={() => onClick(name)} type="button">
+const Button = ({
+  name,
+  onClick,
+  color,
+  wide,
+}) => (
+  <button className={`${color} ${wide}`} onClick={() => onClick(name)} type="button">
     { name }
   </button>
 );
@@ -9,6 +14,13 @@ const Button = ({ name, onClick }) => (
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.string,
+};
+
+Button.defaultProps = {
+  color: 'button-gray',
+  wide: 'button-shape',
 };
 
 export default Button;
--- src/components/button_panel.js
+++ src/components/button_panel.js
@@ -3,34 +3,34 @@ import Button from './button';
 
 const ButtonPanel = ({ onClick }) => (
   <div>
-    <div id="operations">
+    <div id="operations" className="row">
       <Button name="AC" onClick={onClick} />
       <Button name="+/-" onClick={onClick} />
       <Button name="%" onClick={onClick} />
-      <Button name="÷" onClick={onClick} />
+      <Button color="button-orange" name="÷" onClick={onClick} />
     </div>
-    <div className="side_operations">
+    <div className="row">
       <Button name="7" onClick={onClick} />
       <Button name="8" onClick={onClick} />
       <Button name="9" onClick={onClick} />
-      <Button name="x" onClick={onClick} />
+      <Button color="button-orange" name="x" onClick={onClick} />
     </div>
-    <div className="side_operations">
+    <div className="row">
       <Button name="4" onClick={onClick} />
       <Button name="5" onClick={onClick} />
       <Button name="6" onClick={onClick} />
-      <Button name="-" onClick={onClick} />
+      <Button color="button-orange" name="-" onClick={onClick} />
     </div>
-    <div className="side_operations">
+    <div className="row">
       <Button name="1" onClick={onClick} />
       <Button name="2" onClick={onClick} />
       <Button name="3" onClick={onClick} />
-      <Button name="+" onClick={onClick} />
+      <Button color="button-orange" name="+" onClick={onClick} />
     </div>
-    <div className="last_row">
-      <Button name="0" onClick={onClick} />
+    <div className="row">
+      <Button wide="button-zero" name="0" onClick={onClick} />
       <Button name="." onClick={onClick} />
-      <Button name="=" onClick={onClick} />
+      <Button color="button-orange" name="=" onClick={onClick} />
     </div>
   </div>
 );
